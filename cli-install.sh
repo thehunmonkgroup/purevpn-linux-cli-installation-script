faf4592f13fb03e586b59dd432890e38ece49fc8
Chad Phillips <chad@apartmentlines.com>
2023-04-14 12:38:51 -0700

    improvements to Linux installation script $ himBHs custom base dir via  env var himBHs centralize file installation under /pure-linux himBHs separate bin/log dirs himBHs stop pured service before disabling/removing systemd file himBHs output proper PATH modification help

--- cli-install.sh
+++ cli-install.sh
@@ -4,7 +4,8 @@
 DAEMON_VERSION=1.3.0
 CLI_VERSION=2.0.0
 
-APP_DIR=/tmp
+BASE_DIR="${PUREVPN_BASE_DIR:-/opt}"
+APP_DIR="$BASE_DIR/pure-linux"
 DAEMON_FILE_NAME=pured-linux-x64
 DAEMON_COMPRESSED_NAME=$DAEMON_FILE_NAME.gz
 DAEMON_URL=https://purevpn-dialer-assets.s3.amazonaws.com/cross-platform/linux-daemon/$DAEMON_VERSION/$DAEMON_COMPRESSED_NAME
@@ -16,7 +17,8 @@ CLI_APP_URL=https://purevpn-dialer-assets.s3.amazonaws.com/cross-platform/linux-
 ATOM_CONFIG_URL=https://purevpn-dialer-assets.s3.amazonaws.com/cross-platform/atom-update-resolve-conf
 ATOM_CONFIG_WG_URL=https://purevpn-dialer-assets.s3.amazonaws.com/cross-platform/atom-update-resolve-conf-wg
 
-CLI_INSTALLATION_DIR=/etc/pure-linux-cli
+BIN_DIR=$APP_DIR/bin
+LOG_DIR=$APP_DIR/log
 
 install_pre_requisites() {
     echo "Installing the prerequisites"
@@ -36,13 +38,16 @@ setting_up_required_files() {
 
     echo "Configuring required files"
 
-    rm -rf $APP_DIR/atom-update-resolve-conf
+    rm -rf $APP_DIR
+    mkdir -p $APP_DIR
+    mkdir $BIN_DIR
+    mkdir $LOG_DIR
+
     wget --backups=0 --directory-prefix=$APP_DIR $ATOM_CONFIG_URL 
     
     #adding execute permissions
     chmod +x $APP_DIR/atom-update-resolve-conf
 
-    rm -rf $APP_DIR/atom-update-resolve-conf-wg
     wget --backups=0 --directory-prefix=$APP_DIR $ATOM_CONFIG_WG_URL 
     
     #adding execute permissions
@@ -54,19 +59,19 @@ setting_up_daemon() {
     
     if [ "${DAEMON_STATUS}" = "active" ]; then
         echo "stopping daemon!"
+        systemctl stop pured.service
         systemctl disable pured.service
         rm /etc/systemd/system/pured.service
         systemctl daemon-reload
         systemctl reset-failed
     fi
 
-    rm -rf /tmp/$DAEMON_COMPRESSED_NAME
-    wget --backups=0 --directory-prefix=/tmp $DAEMON_URL 
-    yes n | gzip -dv /tmp/$DAEMON_COMPRESSED_NAME
+    wget --backups=0 --directory-prefix=$APP_DIR $DAEMON_URL 
+    yes n | gzip -dv $APP_DIR/$DAEMON_COMPRESSED_NAME
 
-    chmod +x /tmp/$DAEMON_FILE_NAME
+    chmod +x $APP_DIR/$DAEMON_FILE_NAME
 
-    printf "[Unit]\nDescription=purevpn-deamon\nAfter=network.target\n\n[Service]\nExecStart=/tmp/$DAEMON_FILE_NAME --start\nRestart=always\nEnvironment=PATH=/usr/bin:/usr/local/bin\nEnvironment=NODE_ENV=production\nWorkingDirectory=/\nStandardOutput=file:${APP_DIR}/access.log\nStandardError=file:${APP_DIR}/error.log\n        \n[Install]\nWantedBy=multi-user.target" \
+    printf "[Unit]\nDescription=purevpn-deamon\nAfter=network.target\n\n[Service]\nExecStart=$APP_DIR/$DAEMON_FILE_NAME --start\nRestart=always\nEnvironment=PATH=/usr/bin:/usr/local/bin\nEnvironment=NODE_ENV=production\nWorkingDirectory=/\nStandardOutput=file:${LOG_DIR}/access.log\nStandardError=file:${LOG_DIR}/error.log\n        \n[Install]\nWantedBy=multi-user.target" \
             > /etc/systemd/system/pured.service
 
     systemctl daemon-reload
@@ -78,18 +83,13 @@ setting_up_cli() {
     #downloading the installer
     echo $CLI_APP_URL
     
-    rm -rf /tmp/$CLI_COMPRESSED_NAME
-    wget --backups=0 --directory-prefix=/tmp $CLI_APP_URL
-    yes n | gzip -dv /tmp/$CLI_COMPRESSED_NAME
+    wget --backups=0 --directory-prefix=$BIN_DIR $CLI_APP_URL
+    yes n | gzip -dv $BIN_DIR/$CLI_COMPRESSED_NAME
 
-    rm -rf $CLI_INSTALLATION_DIR/
-    mkdir $CLI_INSTALLATION_DIR/
+    chmod +x $BIN_DIR/$CLI_FILE_NAME
 
-    cp /tmp/$CLI_FILE_NAME $CLI_INSTALLATION_DIR/
-    chmod +x $CLI_INSTALLATION_DIR/$CLI_FILE_NAME
-
-    if [[ $PATH != *"${CLI_INSTALLATION_DIR}"* ]]; then
-        PATH=$PATH:$CLI_INSTALLATION_DIR/
+    if [[ $PATH != *"${BIN_DIR}"* ]]; then
+        PATH=$PATH:$BIN_DIR/
     fi
 }
 
@@ -100,6 +100,6 @@ setting_up_daemon
 setting_up_cli
 
 echo "Installation is completed, run the following command to load PureVPN CLI in your profile,"
-echo `echo "PATH=$PATH:$CLI_INSTALLATION_DIR/" \> \~/.bashrc`
+echo "echo \"export PATH=\$PATH:$BIN_DIR\" >> ~/.bashrc"
 
 echo $'\n'Run command \"purevpn-cli --help\" after to get more information on how to use PureVPN CLI
